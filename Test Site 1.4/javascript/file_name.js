eeb2d8d7fa71c52b369c0dcfba2a1c39f720f426
Daniel <danielugulino46@gmail.com>
2021-03-18 14:20:31 -0300

    Reorganização

--- Test Site 1.4/javascript/file_name.js
+++
@@ -1,7 +0,0 @@
-
-function File_Name() {
-    var filename = document.getElementById("news-file");
-    var filelabel = document.getElementById("filelabel");
-    var sla = filename.files[0].name;
-    filelabel.innerHTML = sla
-}
